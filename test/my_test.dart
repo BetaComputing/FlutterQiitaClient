c62a634a55cb7c12104c095d4ea0d982dfaec3c0
aridai <aridai.net@outlook.jp>
2020-10-18 16:59:10 +0900

    空のテストを追加
    テストケースのないテストコードは失敗扱いになるみたい。

--- test/my_test.dart
+++ test/my_test.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:test/test.dart';
+
+void main() {
+  test('テスト', () {
+    const actual = 1 + 2;
+    const expected = 3;
+
+    expect(actual, equals(expected));
+  });
+}
