5cf096e2be27716ec87e4c752d2c63354c7af733
aridai <aridai.net@outlook.jp>
2020-10-19 12:47:34 +0900

    ArticleRepositoryを定義

--- lib/dependency.dart
+++ lib/dependency.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_qiita_client/article/article_repository.dart';
+import 'package:flutter_qiita_client/article/article_repository_impl.dart';
 import 'package:get_it/get_it.dart';
 
 class Dependency {
@@ -6,6 +8,11 @@ class Dependency {
     GetIt.I.registerSingleton<String>(
       DotEnv().env['QIITA_BEARER_TOKEN'],
       instanceName: 'QIITA_BEARER_TOKEN',
+    );
+    GetIt.I.registerLazySingleton<ArticleRepository>(
+      () => ArticleRepositoryImpl(
+        Dependency.resolve(name: 'QIITA_BEARER_TOKEN'),
+      ),
     );
   }
 
